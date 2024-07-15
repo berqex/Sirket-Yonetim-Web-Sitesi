87781c374493d16e2ad29e779f29ccd6dc8222ca
berqex <berkeozdamar_06@hotmail.com>
2024-07-15 22:36:30 +0300

    asp.net proje

---
+++ proje/proje/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace proje.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-9276OGM\\SQLEXPRESS; Database=NETCORE; Trusted_Connection=True;");
+        }
+        public DbSet<departmanlar> _GorevDepartmans { get; set; }
+        public DbSet<personel> _Personels { get; set; }
+        //public DbSet<Admin> Admin { get; set; }
+
+    }
+}
