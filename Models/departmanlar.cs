51fec380f8a04039449bc9c478ee1ddc421a0c5b
berqex <berkeozdamar_06@hotmail.com>
2024-07-15 22:44:52 +0300

    proje

---
+++ Models/departmanlar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace proje.Models
+{
+    public class departmanlar
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+        [StringLength(50)]
+        public string DepartmanAd { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+        [StringLength(250)]
+        public string Detay { get; set; }
+
+        public IList<personel> _Personels { get; set; }
+
+    }
+}
---
+++ Models/personel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace proje.Models
+{
+    public class personel
+    {
+        [Key]
+        public int perid { get; set; }
+        [Column(TypeName = "VARCHAR(5)")]
+        [StringLength(30)]
+        public string ad { set; get; }
+        [Column(TypeName = "VARCHAR(30)")]
+        [StringLength(30)]
+        public string soyad { set; get; }
+        [Column(TypeName = "VARCHAR(30)")]
+        [StringLength(50)]
+        public string sehir { set; get; }
+
+        public int departid { get; set; }
+
+        public departmanlar depart { get; set; }
+
+    }
+}
